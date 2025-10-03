9d9b94496119a9df2160b697423e5dd4262ef95f
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2025-10-03 10:41:25 +0200

    fix: minor fix to tippecanoe runner

--- src/TippecanoeRunner.php
+++ src/TippecanoeRunner.php
@@ -15,6 +15,8 @@ class TippecanoeRunner
             escapeshellarg($mbtilesPath),
             escapeshellarg($geojsonPath)
         );
+        // Just to collapse double space in command if force flag is not used
+        $cmd = preg_replace('/\s+/', ' ', $cmd);
         passthru($cmd, $exitCode);
         if ($exitCode !== 0) {
             throw new RuntimeException("tippecanoe exited with code {$exitCode}");

7dcad3d27e72ccdfb21c70ef723ad7e2827898cb
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-10-15 10:44:52 +0200

    feat: Add final target during mbtiles generation.
    Also added example cron.d entry for map tile server installations
    
    commit b69df48e84518f67d23c88edfb934179b2c6ad7e
    Author: Kåre Slettnes <kaare.slettnes@tromsfylke.no>
    Date:   Wed Oct 15 10:44:12 2025 +0200
    
        convert script alreay has valid shebang.
    
    commit 929270dd4ffc2f3b2079ade63f572463be9f8744
    Author: Kåre Slettnes <kaare.slettnes@tromsfylke.no>
    Date:   Tue Oct 14 15:01:28 2025 +0200
    
        Cron works. It needs to be run as root
    
    commit cbb3308578a86334a5373e301e4d7ae52725b410
    Author: Kåre Slettnes <kaare.slettnes@tromsfylke.no>
    Date:   Tue Oct 14 14:36:21 2025 +0200
    
        Add final destination for successfully generated geojson
    
        Also added non-working cron.d entry. Don't know why it doesn't take.

--- update-stop-tiles.sh
+++ update-stop-tiles.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
 
 #
-# Default configuration. Override by copying .env-example to .env
-#
-# URL containing zipped stop places in NeTEx format.
-# This instance is all active stops in Norway
+# Default configuration. Override by copying .env-example to .env and adjust
+# values there.  See .env-example for explanation
+
 ZIP_URL="https://storage.googleapis.com/marduk-production/tiamat/Current_latest.zip"
-
-# Target of downloaded zip.
 ZIP_FILE="./build/stops.zip"
-
-# Target of generated geojson.
 OUTPUT_GEOJSON="./build/stops.geojson"
-
-# Target of final mbtiles file.
 OUTPUT_MBTILES="./build/stops.mbtiles"
-
-# Set to 1 to force overwrite existing mbtiles file.
 FORCE_MB=1
-
-# Set to 1 if downladed (zip) and temporarily generated files (geojson) files
-# should be deleted after successful tile generation
+TARGET_MBTILE=""
 DELETE_INTERIM=0
 
 cd $(dirname $0)
@@ -29,7 +18,7 @@ if [[ -f ./.env ]]; then
     source ./.env
 fi
 
-PHP_CONVERTER="php ./convert.php"
+PHP_CONVERTER="./convert.php"
 download_file() {
     curl -L -o "$2" "$1" || { echo "Download failed!"; exit 1; }
 }
@@ -38,7 +27,7 @@ run_converter() {
     local cmd="$PHP_CONVERTER --input $1 --output $2"
     if [ -n "$3" ]; then
         cmd="$cmd --mbtiles $3"
-        if [[ $((FORVE_MB)) ]]; then
+        if (( FORCE_MB > 0 )); then
             cmd="$cmd --force"
         fi
     fi
@@ -48,7 +37,21 @@ run_converter() {
 download_file "$ZIP_URL" "$ZIP_FILE"
 run_converter "$ZIP_FILE" "$OUTPUT_GEOJSON" "$OUTPUT_MBTILES"
 
-if [[ $((DELETE_INTERIM)) > 0 ]]; then
-    rm -f $ZIP_FILE
-    rm -f $OUTPUT_GEOJSON
+if (( DELETE_INTERIM > 0 )); then
+    rm -fv $ZIP_FILE
+    rm -fv $OUTPUT_GEOJSON
+fi
+
+CP_OPTS="-v"
+if (( FORCE_MB > 0 )); then
+    CP_OPTS="$CP_OPTS -f"
+else
+    CP_OPTS="$CP_OPTS --update=none"
+fi
+
+if [[ -n $TARGET_MBTILE ]]; then
+    cp $CP_OPTS $OUTPUT_MBTILES $TARGET_MBTILE
+    if (( DELETE_INTERIM > 0 )); then
+        rm -fv $OUTPUT_MBTILES
+    fi
 fi
